c0d07558c9ef6ad260a6122137b0726173296263
Jay Martel <jsmartel68@hotmail.com>
2014-03-15 11:59:22 +0100

    Added pull-updates.sh and cronjob entry

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -5,4 +5,18 @@ class puppet {
     source => 'puppet:///modules/puppet/papply.sh',
     mode   => '0755',
   }
+
+  file {'/usr/local/bin/pull-updates':
+    source => 'puppet:///modules/puppet/pull-updates.sh',
+    mode   => '0755',
+  }
+
+  cron { 'run-puppet':
+    ensure  => 'present',
+    user    => 'ubuntu',
+    command => '/usr/local/bin/pull-updates',
+    minute  => '*/30',
+    hour => '*',
+  }
+
 }
